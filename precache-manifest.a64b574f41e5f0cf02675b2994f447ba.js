b0e712564f65a4fde2e38c4277f391e34c798447
couchp <thecoucher@gmail.com>
2019-10-24 14:00:42 -0400

    Updates

---
+++ precache-manifest.a64b574f41e5f0cf02675b2994f447ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "64eb4994a22864bbb50f",
+    "url": "/react-tutorial/static/js/main.64eb4994.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/react-tutorial/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "64eb4994a22864bbb50f",
+    "url": "/react-tutorial/static/css/main.667147b7.chunk.css"
+  },
+  {
+    "revision": "d62a7721b4ce853b57c171deb7a0b116",
+    "url": "/react-tutorial/index.html"
+  }
+];
