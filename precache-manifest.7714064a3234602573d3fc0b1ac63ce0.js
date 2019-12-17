61cf4e13ba28b3ff9b3545f361cf6b0d49181a92
couchp <thecoucher@gmail.com>
2019-12-17 17:13:59 -0500

    Updates

--- precache-manifest.7714064a3234602573d3fc0b1ac63ce0.js
+++ precache-manifest.7714064a3234602573d3fc0b1ac63ce0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/cribbage1/static/js/runtime~main.daa8ac5a.js"
   },
   {
-    "revision": "879bd62efa89fae65f68",
-    "url": "/cribbage1/static/js/main.879bd62e.chunk.js"
+    "revision": "003465ae79db9b932b78",
+    "url": "/cribbage1/static/js/main.003465ae.chunk.js"
   },
   {
     "revision": "16759eda5b42176a8189",
     "url": "/cribbage1/static/js/1.16759eda.chunk.js"
   },
   {
-    "revision": "879bd62efa89fae65f68",
+    "revision": "003465ae79db9b932b78",
     "url": "/cribbage1/static/css/main.dbf382ab.chunk.css"
   },
   {
-    "revision": "06a7fa63ff3378830e69a7dec74aeedb",
+    "revision": "9b1c6a8e63e124e6c157765a35fd95e4",
     "url": "/cribbage1/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cribbage1/precache-manifest.60870154e568bb31cdab00aee185232e.js"
+  "/cribbage1/precache-manifest.7714064a3234602573d3fc0b1ac63ce0.js"
 );
 
 workbox.clientsClaim();
