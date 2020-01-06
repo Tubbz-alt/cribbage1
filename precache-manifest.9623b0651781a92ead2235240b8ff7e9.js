66dc0f42dde060d620712dc997eb5493bb199d54
couchp <thecoucher@gmail.com>
2020-01-06 16:48:31 -0500

    Updates

---
+++ precache-manifest.9623b0651781a92ead2235240b8ff7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "daa8ac5a11aeae867ab1",
+    "url": "/cribbage1/static/js/runtime~main.daa8ac5a.js"
+  },
+  {
+    "revision": "234847dcf1a42f447bb7",
+    "url": "/cribbage1/static/js/main.234847dc.chunk.js"
+  },
+  {
+    "revision": "da6c2a3aeaf64f2a39b7",
+    "url": "/cribbage1/static/js/1.da6c2a3a.chunk.js"
+  },
+  {
+    "revision": "234847dcf1a42f447bb7",
+    "url": "/cribbage1/static/css/main.9217dce4.chunk.css"
+  },
+  {
+    "revision": "a8f4f02313188ced438c1f79f2ea8044",
+    "url": "/cribbage1/index.html"
+  }
+];
